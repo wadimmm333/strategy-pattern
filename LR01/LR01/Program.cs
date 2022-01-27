aec30bea512f2f1913a5d3b20b9f0b4879194d0f
инет <@KPC.RU>
2022-01-27 10:26:09 +0300

    LR01

--- LR01/LR01/Program.cs
+++ LR01/LR01/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using LR01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AllDucks;
 
 
-namespace LR01
+namespace LR
 {
     class Program
     {
